4d57d1743194bc0a67d9d89b2a725ed204703f97
Diego Covarrubias <dcov@pm.me>
2023-10-12 11:11:10 -0700

    fix addCSourceFiles error

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.Build) void {
 
     lib.linkLibC();
     lib.addIncludePath(.{ .path = "c/include" });
-    lib.addCSourceFiles(&sources, &.{});
+    lib.addCSourceFiles(.{ .files = &sources });
     lib.installHeadersDirectory("c/include/brotli", "brotli");
 
     const target = cross_target.toTarget();
